ac8f88a8d3b41ac1e883f2a0827a9b9349f8a200
David Ambrozio <davidambroziodev@gmail.com>
2023-12-19 14:30:29 -0300

    9 - Arrays e Slices

---
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	//ARRAYS
+	var array1 [5]string
+	array1[0] = "Posição 1"
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	//SLICES
+	slice := []int{13, 213, 412, 23, 1233, 4, 21, 23, 213, 41, 31, 24, 12, 4123, 312}
+	fmt.Println(slice)
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array3))
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posição alterada"
+	fmt.Println(slice2)
+}
