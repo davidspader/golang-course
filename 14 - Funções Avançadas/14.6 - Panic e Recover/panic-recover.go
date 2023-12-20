575d5184d9f10f1bcdfcc804bb65255a7c116cd3
David Ambrozio <davidambroziodev@gmail.com>
2023-12-20 15:28:23 -0300

    14.6 - Panic e Recover

---
+++ 14 - Funções Avançadas/14.6 - Panic e Recover/panic-recover.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func recuperarExecucao() {
+	if r := recover(); r != nil {
+		fmt.Println("Recuperando a execução")
+	}
+}
+
+func alunoEstaAprovado(n1, n2 float32) bool {
+	defer recuperarExecucao()
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("A MÉDIA É EXATAMENTE 6")
+}
+
+func main() {
+	fmt.Println(alunoEstaAprovado(7, 7))
+	fmt.Println("Pós execução")
+
+	fmt.Println(alunoEstaAprovado(6, 6))
+	fmt.Println("Pós execução")
+
+}
