89d1e8420122e7b78eb3c4351e0610ed0de297dd
David Ambrozio <davidambroziodev@gmail.com>
2024-01-01 22:26:08 -0300

    tests com multiplos cenarios

--- 18 - Testes Automatizados/18.1 - Introdução/enderecos/enderecos_test.go
+++ 18 - Testes Automatizados/18.1 - Introdução/enderecos/enderecos_test.go
@@ -2,14 +2,31 @@ package enderecos
 
 import "testing"
 
-func TestTipoDeEndereco(t *testing.T) {
-	enderecoParaTeste := "Avenida paulista"
-	tipoDeEnderecoEsperado := "Avenida"
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
 
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s",
-			tipoDeEnderecoEsperado,
-			tipoDeEnderecoRecebido)
+func TestTipoDeEndereco(t *testing.T) {
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Tipo inválido"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"AVENIDA REBOUÇAS", "Avenida"},
+		{"", "Tipo inválido"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				tipoDeEnderecoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
 	}
 }
