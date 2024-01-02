9712b8ee8d2b5a84054ee3f0d46c897e2a9d0bed
David Ambrozio <davidambroziodev@gmail.com>
2024-01-02 11:14:34 -0300

    19 - JSON - função Marshal

---
+++ 19 - JSON/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type cachorro struct {
+	Nome  string `json:"nome"`
+	Raca  string `json:"raca"`
+	Idade uint   `json:"idade"`
+}
+
+func main() {
+	c := cachorro{"Manu", "Vira-lata", 2}
+
+	cachorroEmJSON, erro := json.Marshal(c)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(cachorroEmJSON)
+	fmt.Println(bytes.NewBuffer(cachorroEmJSON))
+
+	c2 := map[string]string{
+		"nome": "Zuli",
+		"raca": "Vira-lata",
+	}
+
+	cachorro2EmJSON, erro := json.Marshal(c2)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(cachorro2EmJSON)
+	fmt.Println(bytes.NewBuffer(cachorro2EmJSON))
+
+}
