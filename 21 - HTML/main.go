e956b4443001d884746ec950dca9699c9b20b640
David Ambrozio <davidambroziodev@gmail.com>
2024-01-02 11:52:37 -0300

    21 - HTML - creating html

---
+++ 21 - HTML/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/home", home)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Olá Mundo!"))
+}
