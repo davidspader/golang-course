43e064feb8948d4ac0edd18c05b52393d48c9100
David Ambrozio <davidambroziodev@gmail.com>
2024-01-02 15:39:46 -0300

    22 - Banco de dados - Create connection

---
+++ 22 - Banco de dados/banco-de-dados.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	stringConexao := "david:1234@/curso?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conexão aberta!")
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+
+	fmt.Println(linhas)
+}
